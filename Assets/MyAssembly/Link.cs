0473f8f21c1d1968ce733e1bfab91f2c4b4de37d
apitkevichzmeke <a.pitkevich@zmeke.com>
2020-12-03 15:35:06 +0300

    bolt script link

---
+++ Assets/MyAssembly/Link.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link : MonoBehaviour
+{
+
+    [HideInInspector] public int Value = 9822;
+
+}
--- Assets/MyAssembly/TestCustomBoltScript1.cs
+++ Assets/MyAssembly/TestCustomBoltScript1.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TestCustomBoltScript1 : MonoBehaviour
 {
+    public Link Linker;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +13,7 @@ public class TestCustomBoltScript1 : MonoBehaviour
 
     public void DebugNumber(int _x)
     {
-        Debug.Log("next event="+_x);
+        Debug.Log("next event="+_x + " but linker=" + Linker.Value);
     }
 
 }
