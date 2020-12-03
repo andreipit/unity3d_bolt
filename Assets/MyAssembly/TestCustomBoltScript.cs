80c120e10c6e7410c29e021dc648677716ad83c9
apitkevichzmeke <a.pitkevich@zmeke.com>
2020-12-03 14:07:10 +0300

    init

---
+++ Assets/MyAssembly/TestCustomBoltScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCustomBoltScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+
+    public void DebugNumber(int _x)
+    {
+        Debug.Log(_x);
+    }
+
+}
---
+++ Assets/MyAssembly/UIEvents.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+using Ludiq;
+
+
+public class UIEvents : MonoBehaviour
+{
+    
+
+    public bool IsBttnClicked()
+    {
+        //return (bool)Variables.Scene(this).Get("bttnClicked");
+        //return (bool)Variables.Object(this).Get("bttnClicked");
+        //return (bool)Variables.Object(gameObject).Get("bttnClicked");
+        bool oldValue = (bool)Variables.Object(gameObject).Get("bttnClicked22");
+        return oldValue;
+    }
+
+    public void OnBttnClick()
+    {
+        //Variables.Scene(this).Set("bttnClicked22", true);
+        bool oldValue = (bool)Variables.Object(gameObject).Get("bttnClicked22");
+        //Debug.Log(Variables.Object(gameObject).Get("bttnClicked22") + " is now");
+        Variables.Object(gameObject).Set("bttnClicked22", !oldValue);
+    }
+}
