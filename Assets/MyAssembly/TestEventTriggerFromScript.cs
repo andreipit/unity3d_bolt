f0f3bf6e7991910f062f0de1344ce7afaaa5c453
apitkevichzmeke <a.pitkevich@zmeke.com>
2020-12-07 11:28:24 +0300

    bolt event trigger from script

---
+++ Assets/MyAssembly/TestEventTriggerFromScript.cs
@@ -0,0 +1,24 @@
+﻿using Ludiq;
+using Bolt;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEventTriggerFromScript : MonoBehaviour
+{
+    public GameObject flowMachineGameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.T))
+        {
+            CustomEvent.Trigger(flowMachineGameObject, "TestEvent", 350);
+        }
+    }
+}
