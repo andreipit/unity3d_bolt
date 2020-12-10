d260150c782bb33df1d745a29ffccae732776f3c
apitkevichzmeke <a.pitkevich@zmeke.com>
2020-12-10 15:33:01 +0300

    UI events

---
+++ Assets/LinkOfScriptWithoutAssembly.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkOfScriptWithoutAssembly : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void ArbitaryFunction2(int _value)
+    {
+        Debug.Log("ArbitaryFunction2 value = " + _value);
+    }
+}
---
+++ Assets/ScriptWithoutAssembly.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptWithoutAssembly : MonoBehaviour
+{
+    public LinkOfScriptWithoutAssembly link;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void ArbitaryFunction(int _value)
+    {
+        link.ArbitaryFunction2(_value);
+        //Debug.Log("ArbitaryFunction value = " + _value);
+    }
+
+}
